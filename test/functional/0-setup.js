6659e248c9572747284238410a121ae5653bdee4
Nihanth Subramanya <nhnt11@gmail.com>
2019-01-30 19:35:15 +0100

    Clean up repo, condense code, add tests

--- test/functional/0-setup.js
+++ test/functional/0-setup.js
@@ -5,9 +5,12 @@ process.on("unhandledRejection", r => console.error(r)); // eslint-disable-line 
 
 const {assert} = require("chai");
 const utils = require("./utils");
+const variations = require("../../src/variations.json");
 
 const allPrefs = [
   "pref1",
+  "pref2",
+  "pref3",
 ];
 
 async function checkPrefs(driver, prefs) {
@@ -43,105 +46,35 @@ describe("setup and teardown", function() {
     const SETUP_DELAY = 500;
     let addonId;
 
-    describe("sets the correct prefs for variation Control", () => {
-      before(async () => {
-        await utils.setPreference(driver, "extensions.cookie-restrictions_shield_mozilla_org.test.variationName", "Control");
-        addonId = await utils.setupWebdriver.installAddon(driver);
-        await driver.sleep(SETUP_DELAY);
-      });
+    for (const variation in variations) {
+      const prefs = variations[variation].prefs;
+      describe(`sets the correct prefs for variation ${variation}`, () => {
+        before(async () => {
+          await utils.setPreference(driver, "extensions.multipreffer.test.variationName", variation);
+          addonId = await utils.setupWebdriver.installAddon(driver);
+          await driver.sleep(SETUP_DELAY);
+        });
 
-      it("has the correct prefs after install", async () => {
-        await checkPrefs(driver, {
-          "security.pki.distrust_ca_policy": 1,
-          "browser.contentblocking.ui.enabled": false,
-          "browser.fastblock.enabled": false,
+        it("has the correct prefs after install", async () => {
+          await checkPrefs(driver, prefs.setValues);
+        });
+
+        it("has the correct prefs after uninstall", async () => {
+          await utils.setupWebdriver.uninstallAddon(driver, addonId);
+          let prefsToCheck = prefs.setValues;
+          for (const pref of prefs.resetDefaults) {
+            prefsToCheck[pref] = undefined;
+          }
+          for (const pref in prefs.resetValues) {
+            prefsToCheck[pref] = prefs.resetValues[pref];
+          }
+          await checkPrefs(driver, prefsToCheck);
+        });
+
+        after(async () => {
+          await utils.clearPreference(driver, "extensions.multipreffer.test.variationName");
         });
       });
-
-      it("has the correct prefs after uninstall", async () => {
-        await utils.setupWebdriver.uninstallAddon(driver, addonId);
-        await checkPrefs(driver, {});
-      });
-
-      after(async () => {
-        await utils.clearPreference(driver, "extensions.cookie-restrictions_shield_mozilla_org.test.variationName");
-      });
-    });
-
-    describe("sets the correct prefs for variation CookiesBlocked", () => {
-      before(async () => {
-        await utils.setPreference(driver, "extensions.cookie-restrictions_shield_mozilla_org.test.variationName", "CookiesBlocked");
-        addonId = await utils.setupWebdriver.installAddon(driver);
-        await driver.sleep(SETUP_DELAY);
-      });
-
-      it("has the correct prefs after install", async () => {
-        await checkPrefs(driver, {
-          "network.cookie.cookieBehavior": 4,
-          "browser.contentblocking.trackingprotection.ui.enabled": false,
-          "browser.fastblock.enabled": false,
-          "browser.contentblocking.fastblock.ui.enabled": false,
-          "browser.contentblocking.fastblock.control-center.ui.enabled": false,
-          "browser.contentblocking.trackingprotection.control-center.ui.enabled": false,
-          "security.pki.distrust_ca_policy": 1,
-          "browser.contentblocking.enabled": true,
-          "browser.contentblocking.ui.enabled": true,
-          "browser.contentblocking.rejecttrackers.ui.recommended": true,
-          "browser.contentblocking.rejecttrackers.control-center.ui.enabled": true,
-          "browser.contentblocking.cookies-site-data.ui.reject-trackers.recommended": true,
-          "browser.contentblocking.cookies-site-data.ui.reject-trackers.enabled": true,
-          "browser.contentblocking.reportBreakage.enabled": true,
-          "urlclassifier.trackingAnnotationTable": "test-track-simple,base-track-digest256",
-          "urlclassifier.trackingAnnotationWhitelistTable": "test-trackwhite-simple,mozstd-trackwhite-digest256",
-        });
-      });
-
-      it("has the correct prefs after uninstall", async () => {
-        await utils.setupWebdriver.uninstallAddon(driver, addonId);
-        await checkPrefs(driver, {});
-      });
-
-      after(async () => {
-        await utils.clearPreference(driver, "extensions.cookie-restrictions_shield_mozilla_org.test.variationName");
-      });
-    });
-
-    describe("sets the correct prefs for variation AllThirdPartyCookiesBlocked", () => {
-      before(async () => {
-        await utils.setPreference(driver, "extensions.cookie-restrictions_shield_mozilla_org.test.variationName", "AllThirdPartyCookiesBlocked");
-        addonId = await utils.setupWebdriver.installAddon(driver);
-        await driver.sleep(SETUP_DELAY);
-      });
-
-      it("has the correct prefs after install", async () => {
-        await checkPrefs(driver, {
-          "network.cookie.cookieBehavior": 1,
-          "browser.contentblocking.trackingprotection.ui.enabled": false,
-          "browser.fastblock.enabled": false,
-          "browser.contentblocking.fastblock.ui.enabled": false,
-          "browser.contentblocking.fastblock.control-center.ui.enabled": false,
-          "browser.contentblocking.trackingprotection.control-center.ui.enabled": false,
-          "security.pki.distrust_ca_policy": 1,
-          "browser.contentblocking.enabled": true,
-          "browser.contentblocking.ui.enabled": true,
-          "browser.contentblocking.rejecttrackers.ui.recommended": true,
-          "browser.contentblocking.rejecttrackers.control-center.ui.enabled": true,
-          "browser.contentblocking.cookies-site-data.ui.reject-trackers.recommended": true,
-          "browser.contentblocking.cookies-site-data.ui.reject-trackers.enabled": true,
-          "browser.contentblocking.reportBreakage.enabled": true,
-          "urlclassifier.trackingAnnotationTable": "test-track-simple,base-track-digest256",
-          "urlclassifier.trackingAnnotationWhitelistTable": "test-trackwhite-simple,mozstd-trackwhite-digest256",
-        });
-      });
-
-      it("has the correct prefs after uninstall", async () => {
-        await utils.setupWebdriver.uninstallAddon(driver, addonId);
-        await checkPrefs(driver, {});
-      });
-
-      after(async () => {
-        await utils.clearPreference(driver, "extensions.cookie-restrictions_shield_mozilla_org.test.variationName");
-      });
-    });
+    }
   });
 });
